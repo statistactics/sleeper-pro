8d0611e785b72716b14755af0acccbdccf80666c
Caleb Mah <calebmahweekeat@gmail.com>
2025-05-06 16:03:57 +0800

    Starting to add new page

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -47,59 +47,91 @@ class MyAppState extends ChangeNotifier {
 
 
 
+// ...
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected: $value');
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var textPair = appState.current;
+    var pair = appState.current;
 
     IconData icon;
-    if (appState.favourites.contains(textPair)){
+    if (appState.favourites.contains(pair)) {
       icon = Icons.favorite;
-    } else{
+    } else {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('A random idea: testing flutter hot reload feature!'),
-            BigCard(textPair: textPair),
-                      // ↓ Add this.
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                  ElevatedButton.icon(
-                  onPressed: () { 
-                    appState.toggleFavourites();
-                    print(appState.favourites);
-                  },
-                  icon: Icon(icon),
-                  label: Text('Like'), //Note that when using the .icon() constructor, the child arg becomes invalid
-                ),
-
-                SizedBox(width:10),
-
-                ElevatedButton(
-                  onPressed: () {
-                    print('button pressed!');
-                    appState.getNext();
-                  },
-                  child: Text('Next'),
-                ),
-              ],
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(textPair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavourites();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
-      
     );
-
-    
   }
 }
+
+// ...
 
 class BigCard extends StatelessWidget {
   const BigCard({
